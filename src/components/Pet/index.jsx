03e240ce6dc070c409e45b4dcb157f186e17d59a
Gab Silv <gabsilv219@gmail.com>
2023-11-12 20:54:23 -0400

    ready for deploy

--- src/components/Pet/index.jsx
+++ src/components/Pet/index.jsx
@@ -40,71 +40,176 @@ const Pet = () => {
       </div>
       {loading && <p className='loading'>Carregando Dados do Pet...</p>}
       {pet ? (
-        <div className='info'>
-          <div className="info-data">
-            <img className='foto' src={`https://api-seekpet-prisma.onrender.com/pets/${pet.foto}`} alt='foto'/>
-            <div className="info-pet">
-              <h1 className='title-pet'>Informações do Pet:</h1>
-              <div className='data'>
-                <h3>Nome: </h3>
-                <p>{pet.nome}</p>
+        <div>
+          <div>
+            {pet.desaparecido == 'true' && pet.recompensa != null ? (
+              <div>
+                <div className='fieldRow'>
+                  <div className='desaparecido'>
+                    <p className='msgDesaparecido'>PROCURA-SE</p>
+                  </div>
+                  <div className='recompensa'>
+                    <p className='recompensa-text'>Recompensa: {pet.recompensa}</p>
+                  </div>
+                </div>
+                <div className='contato'>
+                  <p className='contato-text'>Entre em contato com o dono</p>
+                </div>
               </div>
-              <div className='data'>
-                <h3>Idade: </h3>
-                <p>{pet.idade}</p>
+            ) : (
+              null
+            )}
+          </div>
+          <div>
+            {pet.desaparecido == 'true' && pet.recompensa == null ? (
+              <div className='fieldRow'>
+                <div className='desaparecido'>
+                  <p className='msgDesaparecido'>PROCURA-SE</p>
+                </div>
               </div>
-              <div className='data'>
-                <h3>Especie: </h3>
-                <p>{pet.especie}</p>
-              </div>
-              <div className='data'>
-                <h3>Raça: </h3>
-                <p>{pet.raca}</p>
-              </div>
-              <div className='data'>
-                <h3>Sexo: </h3>
-                <p>{pet.sexo}</p>
-              </div>
-              <div className='data'>
-                <h3>Doenças: </h3>
-                <p>{pet.doenca}</p>
-              </div>
-              <div className='data'>
-                <h3>Vacinas: </h3>
-                <p>{pet.vacina}</p>
-              </div>
-              <div className='data'>
-                <h3>Castrado: </h3>
-                <p>{pet.castrado}</p>
-              </div>
-            </div>
-                
-            <div className="line"></div>
+            ) : (
+              null
+            )}
+          </div>
+          {pet.desaparecido == 'true' ? (
+            <div className='info-desaparecido'>
+              <div className="info-data">
+                {pet.desaparecido == 'true' ? (
+                  <img className='foto-desaparecido' src={`https://api-seekpet-prisma.onrender.com/pets/${pet.foto}`} alt='foto'/>
+                ) : (
+                  <img className='foto' src={`https://api-seekpet-prisma.onrender.com/pets/${pet.foto}`} alt='foto'/>
+                )}
+                <div className="info-pet">
+                  <h1 className='title-pet'>Informações do Pet:</h1>
+                  <div className='data'>
+                    <h3>Nome: </h3>
+                    <p>{pet.nome}</p>
+                  </div>
+                  <div className='data'>
+                    <h3>Idade: </h3>
+                    <p>{pet.idade}</p>
+                  </div>
+                  <div className='data'>
+                    <h3>Especie: </h3>
+                    <p>{pet.especie}</p>
+                  </div>
+                  <div className='data'>
+                    <h3>Raça: </h3>
+                    <p>{pet.raca}</p>
+                  </div>
+                  <div className='data'>
+                    <h3>Sexo: </h3>
+                    <p>{pet.sexo}</p>
+                  </div>
+                  <div className='data'>
+                    <h3>Doenças: </h3>
+                    <p>{pet.doenca}</p>
+                  </div>
+                  <div className='data'>
+                    <h3>Vacinas: </h3>
+                    <p>{pet.vacina}</p>
+                  </div>
+                  <div className='data'>
+                    <h3>Castrado: </h3>
+                    <p>{pet.castrado}</p>
+                  </div>
+                </div>
+                    
+                <div className="line"></div>
 
-            <div className="info-dono">
-              <h1 className='title-dono'>Informações do Dono:</h1>
-              <div className='data'>
-                <h3>Nome: </h3>
-                <p>{pet.dono.nome}</p>
+                <div className="info-dono">
+                  <h1 className='title-dono'>Informações do Dono:</h1>
+                  <div className='data'>
+                    <h3>Nome: </h3>
+                    <p>{pet.dono.nome}</p>
+                  </div>
+                  <div className='data'>
+                    <h3 className='email'>E-mail: </h3>
+                    <p>{pet.dono.email}</p>
+                  </div>
+                  <div className='data'>
+                    <h3>Telefone: </h3>
+                    <p>{pet.dono.tel}</p>
+                  </div>
+                    <a className='whats__button' href={`https://wa.me/${55}${extrairNumeros(pet.dono.tel)}/?text=Olá, encontrei seu pet ${pet.nome}`} target='blank'>
+                      <AiOutlineWhatsApp className='icon-whats' />
+                      Enviar Mensagem
+                    </a>
+                </div>
               </div>
-              <div className='data'>
-                <h3 className='email'>E-mail: </h3>
-                <p>{pet.dono.email}</p>
+
+            </div>
+          ) : (
+            <div className='info'>
+            <div className="info-data">
+              {pet.desaparecido == 'true' ? (
+                <img className='foto-desaparecido' src={`https://api-seekpet-prisma.onrender.com/pets/${pet.foto}`} alt='foto'/>
+              ) : (
+                <img className='foto' src={`https://api-seekpet-prisma.onrender.com/pets/${pet.foto}`} alt='foto'/>
+              )}
+              <div className="info-pet">
+                <h1 className='title-pet'>Informações do Pet:</h1>
+                <div className='data'>
+                  <h3>Nome: </h3>
+                  <p>{pet.nome}</p>
+                </div>
+                <div className='data'>
+                  <h3>Idade: </h3>
+                  <p>{pet.idade}</p>
+                </div>
+                <div className='data'>
+                  <h3>Especie: </h3>
+                  <p>{pet.especie}</p>
+                </div>
+                <div className='data'>
+                  <h3>Raça: </h3>
+                  <p>{pet.raca}</p>
+                </div>
+                <div className='data'>
+                  <h3>Sexo: </h3>
+                  <p>{pet.sexo}</p>
+                </div>
+                <div className='data'>
+                  <h3>Doenças: </h3>
+                  <p>{pet.doenca}</p>
+                </div>
+                <div className='data'>
+                  <h3>Vacinas: </h3>
+                  <p>{pet.vacina}</p>
+                </div>
+                <div className='data'>
+                  <h3>Castrado: </h3>
+                  <p>{pet.castrado}</p>
+                </div>
               </div>
-              <div className='data'>
-                <h3>Telefone: </h3>
-                <p>{pet.dono.tel}</p>
+                  
+              <div className="line"></div>
+
+              <div className="info-dono">
+                <h1 className='title-dono'>Informações do Dono:</h1>
+                <div className='data'>
+                  <h3>Nome: </h3>
+                  <p>{pet.dono.nome}</p>
+                </div>
+                <div className='data'>
+                  <h3 className='email'>E-mail: </h3>
+                  <p>{pet.dono.email}</p>
+                </div>
+                <div className='data'>
+                  <h3>Telefone: </h3>
+                  <p>{pet.dono.tel}</p>
+                </div>
+                  <a className='whats__button' href={`https://wa.me/${55}${extrairNumeros(pet.dono.tel)}/?text=Olá, encontrei seu pet ${pet.nome}`} target='blank'>
+                    <AiOutlineWhatsApp className='icon-whats' />
+                    Enviar Mensagem
+                  </a>
               </div>
-                <a className='whats__button' href={`https://wa.me/${55}${extrairNumeros(pet.dono.tel)}/?text=Olá, encontrei seu pet ${pet.nome}`} target='blank'>
-                  <AiOutlineWhatsApp className='icon-whats' />
-                  Enviar Mensagem
-                </a>
             </div>
           </div>
-
+          )}
         </div>
       ) : (
-        error && <p>{error}</p>
+        error && <p className='error'>{error}</p>
       )}
 
 
